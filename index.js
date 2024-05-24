1810087ad4c885a3e6dab15d10ba2dc42f55a8b6
Tamas Zeiss <tamas.zeiss@gmail.com>
2024-05-24 07:28:52 +0200

    json serving functionality

--- index.js
+++ index.js
@@ -3,12 +3,27 @@ console.log("Prepare server for running...");
 const express = require('express');
 const app = express();
 const port = 3200;
+const apiEndpoints = [
+  { endpoint: "/api/products", resource: "./data/products.json" },
+  { endpoint: "/api/product-groups", resource: "./data/productGroups.json" },
+  { endpoint: "/api/articles", resource: "./data/articles.json" },
+  { endpoint: "/api/terms", resource: "./data/terms.json" }
+];
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
+});
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
 })
 
-// endpoint url, action
-app.get('/api/products', function (req, res) {
-  res.send('HELLO WORLD');
+apiEndpoints.forEach(apiEndpoint => {
+  app.get(apiEndpoint.endpoint, (req, res) => {
+    const data = require(apiEndpoint.resource);
+    res.json(data);
+  });
 })
